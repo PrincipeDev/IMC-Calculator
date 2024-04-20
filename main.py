d64c7654ca3b8a2bd86e70d4904c69e03ff0cefd
PrincipeDev <borgesdevfront@gmail.com>
2024-04-20 14:41:46 -0300

    Aplicando formatação númerica

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ peso = float(input('Qual é o seu peso em kg: '))
 IMC = peso / (altura / 100) ** 2
 
 if IMC < 18.5:
-  print(f'seu IMC é de {IMC}, e é classificado como Baixo Peso')
+  print(f'seu IMC é de {IMC:.2f}, e é classificado como Baixo Peso')
 elif IMC >= 18.5 and IMC < 24.9:
-  print(f'seu IMC é de {IMC}, e é classificado como Peso Normal')
+  print(f'seu IMC é de {IMC:.2f}, e é classificado como Peso Normal')
 elif IMC >= 25 and IMC < 29.9:
-  print(f'seu IMC é de {IMC}, e é classificado como Excesso de Peso')
+  print(f'seu IMC é de {IMC:.2f}, e é classificado como Excesso de Peso')
 elif IMC >= 30 and IMC < 34.9:
-  print(f'seu IMC é de {IMC}, e é classificado como Obesidade')
+  print(f'seu IMC é de {IMC:.2f}, e é classificado como Obesidade')
 else:
-  print(f'sue IMC é de {IMC}, e é classificado como Obesidade Extrema')
+  print(f'sue IMC é de {IMC:.2f}, e é classificado como Obesidade Extrema')
